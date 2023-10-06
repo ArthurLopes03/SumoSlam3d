af85903d57195cb3239177377bfcef942bb612c2
LukeN25 <luker.noone@gmail.com>
2023-10-06 16:06:55 +0100

    Added Movement Script + Arena Shrink
    Local 2 Player setup and scripting done, added shrinking arena script taken from Arthur.

---
+++ Sumo Slammers 3D/Assets/Scripts/KillMe.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillMe : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private string inputNameHorizontal;
+    [SerializeField] private string inputNameVertical;
+
+    //[SerializeField] private Color color;
+
+    private Rigidbody rb;
+    //private Renderer renderer;
+
+    private float inputHorizontal;
+    private float inputVertical;
+
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //renderer = GetComponentInChildren<Renderer>();
+        //renderer.material.color = color;
+    }
+
+    private void Update()
+    {
+        inputHorizontal = Input.GetAxisRaw(inputNameHorizontal);
+        inputVertical = Input.GetAxisRaw(inputNameVertical);
+
+        Vector3 movementDirection1 = new Vector3(inputHorizontal, 0, inputVertical);
+        movementDirection1.Normalize();
+
+        //transform.Translate(movementDirection1 * Time.deltaTime, Space.World);
+
+        if (movementDirection1 != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection1, Vector3.up);
+
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(inputHorizontal * speed * Time.fixedDeltaTime, rb.velocity.y, inputVertical * speed * Time.fixedDeltaTime);
+    }
+}
