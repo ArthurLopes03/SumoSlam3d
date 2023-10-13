1e4a50a90f657465331712fdccb9c9525e8e8ff5
HomelessPiano <homelesspiano@gmail.com>
2023-10-13 14:54:34 +0100

    Win screen fixed to change - Evan

---
+++ Sumo Slammers 3D/Assets/ChangeText.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeText : MonoBehaviour
+{
+
+
+    public TMP_Text textMesh;
+
+    void Start()
+    {
+
+    }
+}
--- Sumo Slammers 3D/Assets/Scripts/GameManager.cs
+++ Sumo Slammers 3D/Assets/Scripts/GameManager.cs
@@ -6,11 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+    string winner;
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Killed");
-        if(other.tag == "Player")
+        if (other.name == "Player 1")
+        {
             SceneManager.LoadScene(2);
+            /*
+            if(other.name == "Player1")
+            {
+                winner = "Player 2";
+            }
+            else if (other.name == "Player2")
+            {
+                winner = "Player 1";
+            }*/
+        }
+        if (other.name == "Player 2")
+        {
+            SceneManager.LoadScene(3);
+        }
+
     }
 }
