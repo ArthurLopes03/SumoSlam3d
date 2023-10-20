3824317e54e2967cb997eaaa93d67bfe1d7eb0f9
HomelessPiano <homelesspiano@gmail.com>
2023-10-20 18:06:45 +0100

    Revert "Merge branch 'main' of https://github.com/ArthurLopes03/SumoSlam3d"
    This reverts commit 479c5d16e2424d93a7444838e81cc45f2415f280, reversing
    changes made to 7ed04cfe07825470f3f9eeca3210929ba480db32.

--- Sumo Slammers 3D/Assets/Scripts/PlayerPush.cs
+++ Sumo Slammers 3D/Assets/Scripts/PlayerPush.cs
@@ -1,26 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerPush : MonoBehaviour
 {
-    //[SerializeField] private string inputPushButton;
-
-    private float PushButton;
 
     public float pushForce = 10f; // Adjust this value to control the strength of the push.
     
-
-    void Update()
-    {
-        //PushButton = Input.GetAxisRaw(inputPushButton);
-    }
-
     void OnCollisionStay(Collision collision)
     {
         Rigidbody otherRB = collision.gameObject.GetComponent<Rigidbody>();
 
-        if (otherRB != null && Input.GetButton("Fire1"))
+        if (otherRB != null)
         {
             // Calculate the direction from this object to the other object.
             Vector3 direction = collision.transform.position - transform.position;
