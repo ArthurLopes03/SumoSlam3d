b43e9d8e1705c0f626613e0fb703d5b3ac0b53ed
Evan100354 <evanlong100354@gmail.com>
2023-10-22 18:53:27 +0100

    Added Power-Up script
    Basic function atm, only increases player size, power up is in sample scene

---
+++ Sumo Slammers 3D/Assets/Scripts/SizePowerUp.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizePowerUp : MonoBehaviour
+{
+    public float sizeMultiplier = 1.5f;
+    public float duration = 10f;
+    //public GameObject pickupEffect;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine (Pickup(other));
+        }
+        
+    }
+
+    IEnumerator Pickup(Collider player)
+    {
+        //Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        player.transform.localScale *= sizeMultiplier;
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        yield return new WaitForSeconds(duration);
+
+        player.transform.localScale /= sizeMultiplier;
+
+        Destroy(gameObject);
+    }
+}
