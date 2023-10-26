00f5218f6b8a023cae8bf64d134bdba1b63b1d6a
LukeN25 <C22532639@mytudublin.ie>
2023-10-27 00:41:29 +0100

    Animation and Scripting Linkup of dreams
    Man if this doesn't push properly i shall murder my firstborn child.

--- Sumo Slammers 3D/Assets/Scripts/Movement.cs
+++ Sumo Slammers 3D/Assets/Scripts/Movement.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    //Speed, input checking and Animtions
     public static float speed = 200f;
     [SerializeField] private string inputNameHorizontal;
     [SerializeField] private string inputNameVertical;
+
     private Animator animator;
 
-    //[SerializeField] private Color color;
-
     Rigidbody rb;
-    //private Renderer renderer;
 
     private float inputHorizontal;
     private float inputVertical;
@@ -21,40 +20,46 @@ public class Movement : MonoBehaviour
 
     private void Start()
     {
+
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
-        //renderer = GetComponentInChildren<Renderer>();
-        //renderer.material.color = color;
+        
     }
 
     private void Update()
     {
+        //Find the correct inputs
         inputHorizontal = Input.GetAxisRaw(inputNameHorizontal);
         inputVertical = Input.GetAxisRaw(inputNameVertical);
 
+        //Moves character with inputs
         Vector3 movementDirection1 = new Vector3(inputHorizontal, 0, inputVertical);
         movementDirection1.Normalize();
 
-        //transform.Translate(movementDirection1 * Time.deltaTime, Space.World);
 
         if (movementDirection1 != Vector3.zero)
         {
-            animator.SetTrigger("WalkTrigger");
-
             Quaternion toRotation = Quaternion.LookRotation(movementDirection1, Vector3.up);
-
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
-        else
-        {
-            animator.SetTrigger("IdleTrigger");
         }
 
         rb.velocity = new Vector3(inputHorizontal * speed * Time.fixedDeltaTime, rb.velocity.y, inputVertical * speed * Time.fixedDeltaTime);
-    }
 
-    private void FixedUpdate()
-    {
-        //rb.velocity = new Vector3(inputHorizontal * speed * Time.fixedDeltaTime, rb.velocity.y, inputVertical * speed * Time.fixedDeltaTime);
+        // Idle and Walking Animations
+        if (movementDirection1 == Vector3.zero)
+        {
+            // Idle Animation
+            animator.SetFloat("Speed", 0);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 1);
+        }
+
+        // Slap Animation
+        if (Input.GetButton("Fire1Xbox"))
+        {
+            animator.Play("Sumo_Slap");
+        }
     }
 }
